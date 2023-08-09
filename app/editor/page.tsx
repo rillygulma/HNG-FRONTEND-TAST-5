41f412b1acc8f338e7fcf265df253158a511d7b1
matt-o-west <matt.omalley.west@gmail.com>
2023-08-09 18:31:35 -0400

    add and style nav, add editor route

---
+++ app/editor/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../../components/Nav'
+
+const editor = () => {
+  return (
+    <>
+      <Nav />
+      <main className='bg-background'>Main Page</main>
+    </>
+  )
+}
+
+export default editor
---
+++ components/Nav.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Nav = () => {
+  // State to manage the active button in the toggle
+  const [activeButton, setActiveButton] = useState('links') // 'links' or 'profile'
+
+  return (
+    <nav className='flex items-center justify-between p-4 bg-white'>
+      {/* Logo */}
+      <Image
+        src='./images/logo-devlinks-small.svg'
+        alt='DevLinks Logo'
+        className='h-8 w-auto'
+        width={100}
+        height={100}
+      />
+
+      {/* Toggle buttons */}
+      <div className='flex'>
+        <button
+          className={`px-4 py-2 ${
+            activeButton === 'links'
+              ? 'bg-tertiary.blue text-white rounded-lg'
+              : 'text-black'
+          }`}
+          onClick={() => setActiveButton('links')}
+        >
+          <Image
+            src='/images/icon-links-header.svg'
+            alt='Links Icon'
+            className='h-6 w-auto'
+            width={100}
+            height={100}
+          />
+        </button>
+        <button
+          className={`px-4 py-2 ${
+            activeButton === 'profile'
+              ? 'bg-tertiary.blue text-white rounded-lg'
+              : 'text-black'
+          }`}
+          onClick={() => setActiveButton('profile')}
+        >
+          <Image
+            src='./images/icon-profile-details-header.svg'
+            alt='Profile Details Icon'
+            className='h-6 w-auto'
+            width={100}
+            height={100}
+          />
+        </button>
+      </div>
+
+      {/* Preview button */}
+      <button className='px-4 py-2 text-white'>
+        <Image
+          src='./images/icon-preview-header.svg'
+          alt='Preview Icon'
+          className='h-6 w-auto'
+          width={100}
+          height={100}
+        />
+      </button>
+    </nav>
+  )
+}
+
+export default Nav
