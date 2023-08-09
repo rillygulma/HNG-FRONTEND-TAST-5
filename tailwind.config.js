748d780c2bc336b820732179e565d86fb6f1a217
matt-o-west <matt.omalley.west@gmail.com>
2023-08-09 17:45:01 -0400

    add and style login route

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,39 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: '#333333',
+        'primary.gray': '#737373',
+        'secondary.gray': '#D9D9D9',
+        'tertiary.gray': '#FAFAFA',
+        'primary.blue': '#633CFF',
+        'secondary.blue': '#BEADFF',
+        'tertiary.blue': '#EFEBFF',
+        background: '#FAFAFA',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['InstrumentSans', 'sans-serif'],
+      },
+      screens: {
+        phone: '320px',
+        // => @media (min-width: 320px) { ... }
+
+        lgPhone: '420px',
+        // => @media (min-width: 420px) { ... }
+
+        tablet: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        laptop: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: '1280px',
+        // => @media (min-width: 1280px) { ... }
       },
     },
   },
