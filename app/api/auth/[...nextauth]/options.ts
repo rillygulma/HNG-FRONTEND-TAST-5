f8365c446a710cf8d075239fa5683dd8e16682b3
matt-o-west <matt.omalley.west@gmail.com>
2023-08-19 22:57:44 -0400

    login action working

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -13,8 +13,7 @@ export const options: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     CredentialsProvider({
-      name: 'Credentials',
-
+      name: 'credentials',
       credentials: {
         email: { label: 'Email', type: 'text' },
         password: { label: 'Password', type: 'password' },
@@ -23,11 +22,17 @@ export const options: NextAuthOptions = {
       async authorize(credentials) {
         //Retrieve user data here.
         //Refer to https://next-auth.js.org/configuration/providers/credentials
+
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('No credentials')
+        }
+
         const user = await db.user.findUnique({
           where: {
             email: credentials?.email,
           },
         })
+
         if (
           credentials?.email === user?.email &&
           credentials?.password === user?.password
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
+'use client'
 import Image from 'next/image'
+import { signIn } from 'next-auth/react'
+import { useState } from 'react'
+import Link from 'next/link'
 
-export default function Home() {
+export default function SignIn() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+  const loginUser = async (e) => {
+    e.preventDefault()
+    console.log('login function runs')
+    signIn('credentials', {
+      ...data,
+      callbackUrl: '/editor',
+    }).then(() => console.log('login successful'))
+  }
+
+  const test = () => {
+    console.log('test submission')
+  }
+
   return (
     <main className='min-h-screen flex items-center justify-center bg-background'>
       <div className='w-full max-w-md desktop:mb-0 phone:mb-16'>
         <div className='flex items-center justify-center mb-6 phone:aboslute phone:top-10'>
           <Image
             src='./images/logo-devlinks-large.svg'
-            alt=''
+            alt='Devlinks Logo'
             width={200}
             height={200}
             className='mb-1'
@@ -16,7 +37,7 @@ export default function Home() {
 
         <div className='p-6 pb-8 mx-6 phone:mx-2'>
           <h1 className='text-2xl font-bold text-black mb-6'>Login</h1>
-          <form>
+          <form action='' onSubmit={loginUser}>
             <div className='mb-4'>
               <label
                 className='block text-sm font-medium text-black mb-2'
@@ -29,6 +50,7 @@ export default function Home() {
                 id='username'
                 type='text'
                 placeholder='Enter your username'
+                onChange={(e) => setData({ ...data, email: e.target.value })}
               />
             </div>
 
@@ -44,6 +66,7 @@ export default function Home() {
                 id='password'
                 type='password'
                 placeholder='Enter your password'
+                onChange={(e) => setData({ ...data, password: e.target.value })}
               />
             </div>
 
@@ -54,15 +77,14 @@ export default function Home() {
               Login
             </button>
           </form>
-
           <div className='flex mt-4 justify-center flex-wrap'>
             <span className='text-black'>Don&apos;t have an account?</span>
-            <a
-              href='#'
+            <Link
+              href='/register'
               className='desktop:block phone:flex phone:flex-nowrap justify-center ml-2 text-primary.blue hover:text-secondary.blue'
             >
               Create one
-            </a>
+            </Link>
           </div>
         </div>
       </div>
