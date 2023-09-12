ed06e7d50bffbd445ab61f19ddacb00cb3254b73
matt-o-west <matt.omalley.west@gmail.com>
2023-09-11 21:24:19 -0400

    working on links CRUD and styling

--- components/EditLinkBlock.tsx
+++ components/EditLinkBlock.tsx
@@ -14,21 +14,64 @@ type LinkBlockProps = {
   removeLink: (index: number) => void
 }
 
+type PlatformKeys =
+  | 'default'
+  | 'github'
+  | 'frontendmentor'
+  | 'x'
+  | 'linkedin'
+  | 'youtube'
+  | 'facebook'
+  | 'twitch'
+  | 'devto'
+  | 'codewars'
+  | 'codepen'
+  | 'freecodecamp'
+  | 'gitlab'
+  | 'hashnode'
+  | 'stackoverflow'
+
+type PlatformOptions = {
+  [K in PlatformKeys]: string
+}
+
+const platformOptions: PlatformOptions = {
+  default: 'Select a platform',
+  github: 'GitHub',
+  frontendmentor: 'Frontend Mentor',
+  x: 'X',
+  linkedin: 'LinkedIn',
+  youtube: 'YouTube',
+  facebook: 'Facebook',
+  twitch: 'Twitch',
+  devto: 'Dev.to',
+  codewars: 'Codewars',
+  codepen: 'Codepen',
+  freecodecamp: 'freeCodeCamp',
+  gitlab: 'GitLab',
+  hashnode: 'Hashnode',
+  stackoverflow: 'Stack Overflow',
+}
+
 const EditLinkBlock = ({
   link,
   index,
   updateLink,
   removeLink,
 }: LinkBlockProps) => {
-  const [platform, setPlatform] = useState(link.platform)
-  const [url, setUrl] = useState(link.url)
+  const [platform, setPlatform] = useState(link.platform || '')
+  const [url, setUrl] = useState(link.url || '')
 
   useEffect(() => {
     updateLink(index, { id: link.id, platform, url })
   }, [platform, url])
 
+  const keys = Object.keys(platformOptions) as Array<
+    keyof typeof platformOptions
+  >
+
   return (
-    <article className='text-primary.gray text-sm z-50 bg-background rounded-md h-56 w-full my-4'>
+    <article className='text-primary.gray text-sm z-50 bg-background rounded-md h-60 w-full my-4'>
       <div className='flex justify-between py-4 px-3'>
         <div className=' flex w-full'>
           <Image
@@ -51,30 +94,26 @@ const EditLinkBlock = ({
           Remove
         </button>
       </div>
-      <div className='px-4 space-y-4'>
+      <div className='px-4 space-y-5'>
         <div>
           <label htmlFor='platform'> Platform</label>
           <select
             name='platform'
-            className='w-full px-4 py-2 border rounded-md  text-black placeholder-primary.gray bg-tertiary.gray'
+            className='w-full px-4 py-2 mt-2 border rounded-md  text-black placeholder-primary.gray bg-white'
             value={platform}
             onChange={(e) => setPlatform(e.target.value)}
           >
-            <option value=''>Select a platform</option>
-            <option value=''>GitHub</option>
-            <option value=''>Frontend Mentor</option>
-            <option value=''>X</option>
-            <option value=''>LinkedIn</option>
-            <option value=''>YouTube</option>
-            <option value=''>Facebook</option>
-            <option value=''>Twitch</option>
-            <option value=''>Dev.to</option>
-            <option value=''>Codewars</option>
-            <option value=''>Codepen</option>
-            <option value=''>freeCodeCamp</option>
-            <option value=''>GitLab</option>
-            <option value=''>Hashnode</option>
-            <option value=''>Stack Overflow</option>
+            {keys.map((key, i) => (
+              <option value={key} key={i}>
+                <Image
+                  src={`./images/icon-${key}.svg`}
+                  width={22}
+                  height={22}
+                  alt=''
+                />
+                {platformOptions[key]}
+              </option>
+            ))}
           </select>
         </div>
         <div>
@@ -82,7 +121,7 @@ const EditLinkBlock = ({
           <input
             name='link'
             type='url'
-            className='w-full px-4 py-2 border rounded-md text-black placeholder-primary.gray bg-tertiary.gray'
+            className='input-with-icon w-full px-4 py-2 mt-2 border rounded-md text-black placeholder-primary.gray bg-white'
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
