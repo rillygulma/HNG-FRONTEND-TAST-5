ae26687f097319b57659830ea36295e24a01beac
matt-o-west <matt.omalley.west@gmail.com>
2023-09-23 00:04:05 -0400

    remigrate db for profileImage field, add file upload logic at route handler

--- app/api/profile/route.tsx
+++ app/api/profile/route.tsx
@@ -4,58 +4,12 @@ import { getServerSession } from 'next-auth'
 import { options } from '../../api/auth/[...nextauth]/options'
 
 import { db } from '@/prisma/db.server'
-import { NextApiRequest } from 'next'
 import multer from 'multer'
 import { s3Client } from '@/libs/awsClient.js'
 import { PutObjectCommand } from '@aws-sdk/client-s3'
-import { S3RequestPresigner, getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { createPresignedPost } from '@aws-sdk/s3-presigned-post'
-
-import nextConnect from 'next-connect'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const upload = multer({ storage: multer.memoryStorage() })
-
-const uploadFile = async (file: Express.Multer.File) => {
-  const params = {
-    Bucket: process.env.AWS_BUCKET,
-    Key: file.originalname,
-    Body: file.buffer,
-  }
-
-  try {
-    await s3Client.send(new PutObjectCommand(params))
-    return { success: true }
-  } catch (err) {
-    return { success: false, error: err }
-  }
-}
-
-const handler = nextConnect()
-  .use(upload.single('file'))
-  .post(async (req, res) => {
-    const file = req.file
-    const key = Date.now().toString() + '-' + file.originalname
-
-    const putParams = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: key,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-      ACL: 'public-read',
-    }
-
-    try {
-      await s3Client.send(new PutObjectCommand(putParams))
-
-      const signedUrl = await createRequestPresigner(s3Client)
-      const url = signedUrl(putParams, { expiresIn: 60 * 60 * 1000 }) // 1 hour
-
-      res.status(200).json({ url })
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Error uploading file to S3' })
-    }
-  })
 
 export async function POST(req: Request, res: Response) {
   const session = await getServerSession(options)
@@ -75,6 +29,58 @@ export async function POST(req: Request, res: Response) {
     return NextResponse.json(
       { error: 'User not found', errorType: 'TOAST_ERROR' },
       { status: 404 }
+    )
+  }
+
+  // upload profile image to aws
+  const processFileUpload = (req, res) => {
+    return new Promise((resolve, reject) => {
+      const multerSingle = upload.single('file')
+      multerSingle(req, res, async (err) => {
+        if (err) {
+          return reject(err)
+        }
+        if (req.file) {
+          const putObjectParams = {
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: req.file.originalname,
+            Body: req.file.buffer,
+          }
+          const putObjectCommand = new PutObjectCommand(putObjectParams)
+          const signedUrl = await getSignedUrl(s3Client, putObjectCommand, {
+            expiresIn: 3600,
+          })
+
+          // signed url that gets saved to db
+          return resolve(signedUrl)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+
+  try {
+    const signedUrl = await processFileUpload(req, res)
+    console.log(signedUrl)
+
+    if (signedUrl) {
+      const image = await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          profileImage: signedUrl,
+        },
+      })
+      console.log(image)
+    } else {
+      return NextResponse.json({ error: 'No image provided' }, { status: 400 })
+    }
+  } catch {
+    return NextResponse.json(
+      { error: "Can't upload image, try again later." },
+      { status: 500 }
     )
   }
 
